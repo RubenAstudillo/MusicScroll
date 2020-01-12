28c41e51adc716fd47f0cc1542be92daf50fa942
Ruben Astudillo <ruben.astud@gmail.com>
2020-01-12 06:13:44 -0300

    getGtkScene can only be called inside the gtk thread
    Otherwise we get a cryptic error that doesn't help at all.

--- src/MusicSorter/UI.hs
+++ src/MusicSorter/UI.hs
@@ -1,18 +1,20 @@
 {-# language NamedFieldPuns, RecordWildCards, OverloadedStrings, BangPatterns #-}
 module MusicSorter.UI (setupUIThread) where
 
-import Control.Monad (forever)
-import Control.Concurrent.Async (Async, withAsyncBound, withAsync)
+import           Control.Concurrent.Async (Async, withAsyncBound, withAsync)
 import qualified Control.Concurrent.Async as A
-import Control.Concurrent.STM (STM, atomically)
-import Control.Concurrent.STM.TBQueue (TBQueue, readTBQueue)
-import Data.GI.Gtk.Threading (setCurrentThreadAsGUIThread, postGUISync)
-import Data.Functor (void)
-import Data.Maybe (fromJust)
-import Data.Text (Text)
-import Data.Text as T
+import           Control.Concurrent.STM (STM, atomically)
+import           Control.Concurrent.STM.TBQueue (TBQueue, readTBQueue)
+import           Control.Concurrent.STM.TMVar (TMVar, newEmptyTMVar, takeTMVar, putTMVar)
+import qualified Control.Concurrent.STM.TMVar as TM
+import           Control.Monad (forever)
+import           Data.Functor (void)
+import           Data.GI.Gtk.Threading (setCurrentThreadAsGUIThread, postGUISync)
+import           Data.Maybe (fromJust)
+import           Data.Text (Text)
+import           Data.Text as T
 import qualified GI.Gtk as Gtk
-import MusicSorter.MPRIS (TrackInfo(..))
+import           MusicSorter.MPRIS (TrackInfo(..))
 
 import Paths_musicSorter
 
@@ -39,30 +41,32 @@ getGtkScene = do
 
 setupUIThread :: TBQueue (TrackInfo, [Text]) -> IO ()
 setupUIThread trackUpdates =
-  do appCtx <- getGtkScene
-     withAsyncBound (uiThread appCtx) $ \a1 ->
-       A.withAsync (uiUpdateThread trackUpdates appCtx) $ \a2 ->
+  do appCtxMVar <- atomically newEmptyTMVar
+     withAsyncBound (uiThread appCtxMVar) $ \a1 ->
+       A.withAsync (uiUpdateThread trackUpdates appCtxMVar) $ \a2 ->
          void $ A.wait a1 <* A.wait a2
 
-uiThread :: AppContext -> IO ()
-uiThread AppContext {..} = do
+uiThread :: TMVar AppContext -> IO ()
+uiThread ctxMVar = do
   setCurrentThreadAsGUIThread
   _ <- Gtk.init Nothing
-  -- AppContext {..} <- getGtkScene
+  appCtx@(AppContext {..}) <- getGtkScene
+  atomically (putTMVar ctxMVar appCtx)
   Gtk.labelSetText titleLabel "Hola Mundo"
   Gtk.widgetShowAll mainWindow
   Gtk.onWidgetDestroy mainWindow Gtk.mainQuit
   Gtk.main
 
 ---
-
-uiUpdateThread :: TBQueue (TrackInfo, [Text]) -> AppContext -> IO a
-uiUpdateThread input appCtx = forever $
-  do (track, lyrics) <- atomically (readTBQueue input)
-     let !lyricsViewRef  = lyricsTextView appCtx
-         !singleLyrics  = T.unlines lyrics
-         !bytesToUpdate = fromIntegral $ T.length singleLyrics
-     postGUISync $ do
-       lyricsBuffer <- Gtk.textViewGetBuffer lyricsViewRef
-       Gtk.textBufferSetText lyricsBuffer singleLyrics bytesToUpdate
+uiUpdateThread :: TBQueue (TrackInfo, [Text]) -> TMVar AppContext -> IO a
+uiUpdateThread input ctxMVar = do
+  appCtx <- atomically (takeTMVar ctxMVar)
+  forever $
+      do (track, lyrics) <- atomically (readTBQueue input)
+         let !lyricsViewRef  = lyricsTextView appCtx
+             !singleLyrics  = T.unlines lyrics
+             !bytesToUpdate = fromIntegral $ T.length singleLyrics
+         postGUISync $ do
+           lyricsBuffer <- Gtk.textViewGetBuffer lyricsViewRef
+           Gtk.textBufferSetText lyricsBuffer singleLyrics bytesToUpdate
 
