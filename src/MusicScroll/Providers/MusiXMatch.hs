d0c181856a2f37915fcb70ea95518d1982884ed2
Ruben Astudillo <ruben.astud@gmail.com>
2020-04-05 17:55:42 -0400

    Allow lyrics with unsafe contents from xmatch

--- src/MusicScroll/Providers/MusiXMatch.hs
+++ src/MusicScroll/Providers/MusiXMatch.hs
@@ -6,8 +6,9 @@ import Data.Maybe (catMaybes)
 import Data.Text (Text, replace, toTitle)
 import Network.HTTP.Req
 import Text.HTML.TagSoup
-import Text.HTML.TagSoup.Match (tagOpenAttrLit)
+import Text.HTML.TagSoup.Match (tagOpenLit, anyAttr)
 import Data.Traversable (mapAccumL)
+import qualified Data.Set as Set
 -- import Data.Text.IO as T (readFile)
 
 import MusicScroll.TrackInfo (TrackInfo(..))
@@ -40,12 +41,15 @@ pipeline :: Text -> Lyrics
 pipeline = parseTags >>> mapAccumL discriminate False
              >>> snd >>> catMaybes >>> innerText >>> Lyrics
 
+--
 discriminate :: Bool -> Tag Text -> (Bool, Maybe (Tag Text))
 discriminate onSpan@True tag | isTagText tag = (onSpan, pure tag)
 discriminate onSpan tag
-  | tagOpenAttrLit "span" spanDiscr tag = (True, Nothing)
-  | isTagCloseName "span" tag           = (False, Nothing)
-  | otherwise                           = (onSpan, Nothing)
+  | tagOpenLit "span" goodClasses tag = (True, Nothing)
+  | isTagCloseName "span" tag         = (False, Nothing)
+  | otherwise                         = (onSpan, Nothing)
   where
-    spanDiscr = ("class", "lyrics__content__ok")
+    goodClasses = anyAttr (\attr -> Set.member attr spanDiscr)
+    spanDiscr = Set.fromList [ ("class", "lyrics__content__ok")
+                             , ("class", "lyrics__content__warning") ]
 
